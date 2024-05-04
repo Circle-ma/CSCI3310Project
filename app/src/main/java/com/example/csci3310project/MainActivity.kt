e3fde5e3f68cafd74267c2c8386fc8e13ec63e0c
Circle-ma <a859576084@gmail.com>
2024-05-04 18:16:17 +0800

    add top bar and bottom bar

--- app/src/main/java/com/example/csci3310project/MainActivity.kt
+++ app/src/main/java/com/example/csci3310project/MainActivity.kt
@@ -6,24 +6,48 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.csci3310project.ui.theme.CSCI3310ProjectTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CSCI3310ProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(topBar = {
+                    TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ), title = {
+                        Text("Small Top App Bar")
+                    })
+                }, bottomBar = {
+                    BottomAppBar {
+                        IconButton(onClick = {}) {
+                            Icon(
+                                Icons.Rounded.ShoppingCart,
+                                contentDescription = "Shopping Cart Icon"
+                            )
+                        }
+                    }
+                }, modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -33,8 +57,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
