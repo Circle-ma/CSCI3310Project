8f01a28f2cc8a43bd8361d4f80ec5876f92d4e37
CKH3 <1155158622@link.cuhk.edu.hk>
2024-05-10 21:31:44 +0800

    Update Expense with real-time exchange rate

---
+++ app/src/main/java/com/example/csci3310project/ExchangeRateManager.kt
@@ -0,0 +1,47 @@
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.example.csci3310project.ExchangeRates
+import com.google.gson.Gson
+
+class ExchangeRateManager(private val queue: RequestQueue) {
+    private val gson = Gson()
+    private val url = "https://v6.exchangerate-api.com/v6/637178fd9b6dcad278507450/latest/"
+    private val exchangeRatesCache = mutableMapOf<String, ExchangeRates>()
+
+    fun getExchangeRates(baseCurrency: String, callback: (ExchangeRates) -> Unit) {
+        if (exchangeRatesCache.containsKey(baseCurrency)) {
+            callback(exchangeRatesCache[baseCurrency]!!)
+        } else {
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET,
+                "$url$baseCurrency",
+                null,
+                { response ->
+                    val exchangeRates = gson.fromJson(response.toString(), ExchangeRates::class.java)
+                    exchangeRatesCache[baseCurrency] = exchangeRates
+                    callback(exchangeRates)
+                },
+                { error ->
+                    error.printStackTrace()
+                    Log.e("ExchangeRateManager", "Error: ${error.networkResponse.statusCode}")
+                }
+            )
+
+            queue.add(jsonObjectRequest)
+        }
+    }
+
+    fun convertCurrency(
+        exchangeRates: ExchangeRates,
+        baseCurrency: String,
+        targetCurrency: String,
+        amount: Double,
+        callback: (Double) -> Unit
+    ) {
+        val rate = exchangeRates.conversion_rates[baseCurrency] ?: 0.0
+        callback(amount / rate)
+    }
+
+}
